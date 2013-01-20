d179eb6177088ab319b2c8d40e97dfb73b542290
nikhilchorappan <nikhilchorappan@gmail.com>
2013-01-20 14:38:17 +0530

    made git ignore

--- app.py
+++ app.py
@@ -12,17 +12,14 @@ def login():
 	else:
 		return valid_login( request.form['username'],request.form['password'])
  
-
 def valid_login(username,password):
    db = losoDB()
    if db.authenticate(username,password) :
-	  db = losoDB()
-	
-	  data = db.gettable('tweets')
+	  db = losoDB()	
+	  data = db.gethomedata()	  
 	  return render_template('homepage.html',name = username, table = data)
    else :
 	  return render_template('login.html',error = " Authentication faild" )
-
 
 @app.route('/sign_up', methods=['POST','GET'])
 
@@ -43,8 +40,8 @@ def tipreader(username):
 		db = losoDB()
 		taglist = request.form['taglist'].rsplit(' ')
 		db.addtip( request.form['username'], request.form['body'],
-		 request.form['latitude'],request.form['longitude'],
-	        0, taglist)
+		   request.form['latitude'],request.form['longitude'],
+	           0, taglist)
 	        return render_template('homepage.html', name = username)     			
  	else:										    
 		return render_template('tipreader.html', name = username) 
@@ -71,7 +68,6 @@ def search_tweet(username,search,lattitude,longitude):
 	return render_template('showtable.html',table = data)
 
 
-	
 
 if __name__ == '__main__':
 	app.run(debug = True)
